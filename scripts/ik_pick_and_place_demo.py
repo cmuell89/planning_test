51b07ddabc30b2fb06fbfb90a9234ce632e8e055
Carl Mueller <cmuell89@gmail.com>
2018-03-27 19:26:54 -0600

    crappy commit message!

--- scripts/ik_pick_and_place_demo.py
+++ scripts/ik_pick_and_place_demo.py
@@ -21,6 +21,7 @@ import argparse
 import struct
 import sys
 import copy
+from collections import OrderedDict
 
 import rospy
 import rospkg
@@ -37,14 +38,58 @@ from geometry_msgs.msg import (
 )
 
 import intera_interface
+from sawyer_interface.moveit_interface import SawyerMoveitInterface
+
+
+class SimulationObject(object):
+    def __init__(self, name, model_filepath, model_type, pose,
+                 reference_frame="world", hover_distance=.15,
+                 overhead_orientation=Quaternion(
+                             x=-0.011940598492311645,
+                             y=0.9990539733303562,
+                             z=-0.007206884011932187,
+                             w=0.04119030593855785)):
+        self.name = name
+        self.filepath = model_filepath
+        self.model_type = model_type
+        self.pose = pose
+        self.reference_frame = reference_frame
+        self.hover_distance = .15
+        self.overhead_orientation = overhead_orientation
+
+    def load_gazebo_model(self):
+        # Load Table SDF
+        xml = ''
+        with open(self.filepath, "r") as file:
+            xml = file.read().replace('\n', '')
+        if self.model_type == "sdf":
+            # Spawn Table SDF
+            rospy.wait_for_service('/gazebo/spawn_sdf_model')
+            try:
+                spawn_sdf = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
+                resp_sdf = spawn_sdf(self.name, xml, "/", self.pose, self.reference_frame)
+            except rospy.ServiceException, e:
+                rospy.logerr("Spawn SDF service call failed: {0}".format(e))
+        elif self.model_type == "urdf":
+            # Spawn Red Block URDF
+            rospy.wait_for_service('/gazebo/spawn_urdf_model')
+            try:
+                spawn_urdf = rospy.ServiceProxy('/gazebo/spawn_urdf_model', SpawnModel)
+                resp_urdf = spawn_urdf(self.name, xml, "/", self.pose, self.reference_frame)
+            except rospy.ServiceException, e:
+                rospy.logerr("Spawn URDF service call failed: {0}".format(e))
+
+    def get_current_pose(self):
+        return copy.deepcopy(self.pose)
+
 
 class PickAndPlace(object):
-    def __init__(self, limb="right", hover_distance = 0.15, tip_name="right_gripper_tip"):
-        self._limb_name = limb # string
-        self._tip_name = tip_name # string
-        self._hover_distance = hover_distance # in meters
+    def __init__(self, limb="right", tip_name="right_gripper_tip"):
+        self._limb_name = limb  # string
+        self._tip_name = tip_name  # string
         self._limb = intera_interface.Limb(limb)
         self._gripper = intera_interface.Gripper()
+
         # verify robot is enabled
         print("Getting robot state... ")
         self._rs = intera_interface.RobotEnable(intera_interface.CHECK_VERSION)
@@ -52,12 +97,17 @@ class PickAndPlace(object):
         print("Enabling robot... ")
         self._rs.enable()
 
+        self._moveit_interface = SawyerMoveitInterface()
+        self._moveit_interface.set_velocity_scaling(.5)
+        self._moveit_interface.set_acceleration_scaling(.5)
+
     def move_to_start(self, start_angles=None):
         print("Moving the {0} arm to start pose...".format(self._limb_name))
         if not start_angles:
             start_angles = dict(zip(self._joint_names, [0]*7))
         self._guarded_move_to_joint_position(start_angles)
         self.gripper_open()
+        rospy.loginfo("Current position: {}".format(self._limb.endpoint_pose()))
 
     def _guarded_move_to_joint_position(self, joint_angles, timeout=5.0):
         if rospy.is_shutdown():
@@ -75,152 +125,59 @@ class PickAndPlace(object):
         self._gripper.close()
         rospy.sleep(1.0)
 
-    def _approach(self, pose):
-        approach = copy.deepcopy(pose)
+    def _approach(self, sim_object):
+        rospy.loginfo("Approaching {}".format(sim_object.name))
+        approach_pose = sim_object.get_current_pose()
         # approach with a pose the hover-distance above the requested pose
-        approach.position.z = approach.position.z + self._hover_distance
-        joint_angles = self._limb.ik_request(approach, self._tip_name)
-        self._limb.set_joint_position_speed(0.001)
-        self._guarded_move_to_joint_position(joint_angles)
-        self._limb.set_joint_position_speed(0.1)
+        approach_pose.position.z = approach_pose.position.z + sim_object.hover_distance
+        approach_pose.orientation = sim_object.overhead_orientation
+        rospy.loginfo("Approach pose: {}".format(approach_pose))
+        self._moveit_interface.move_to_pose_target(approach_pose)
 
-    def _retract(self):
-        # retrieve current pose from endpoint
-        current_pose = self._limb.endpoint_pose()
-        ik_pose = Pose()
-        ik_pose.position.x = current_pose['position'].x
-        ik_pose.position.y = current_pose['position'].y
-        ik_pose.position.z = current_pose['position'].z + self._hover_distance
-        ik_pose.orientation.x = current_pose['orientation'].x
-        ik_pose.orientation.y = current_pose['orientation'].y
-        ik_pose.orientation.z = current_pose['orientation'].z
-        ik_pose.orientation.w = current_pose['orientation'].w
-        self._servo_to_pose(ik_pose)
+    def _retract(self, sim_object):
+        target_pose = sim_object.get_current_pose()
+        target_pose.position.z = target_pose.position.z + sim_object.hover_distance
+        target_pose.orientation = sim_object.overhead_orientation
+        rospy.loginfo("Retract pose: {}".format(target_pose))
 
-    def _servo_to_pose(self, pose, time=4.0, steps=400.0):
-        ''' An *incredibly simple* linearly-interpolated Cartesian move '''
-        r = rospy.Rate(1/(time/steps)) # Defaults to 100Hz command rate
-        current_pose = self._limb.endpoint_pose()
-        ik_delta = Pose()
-        ik_delta.position.x = (current_pose['position'].x - pose.position.x) / steps
-        ik_delta.position.y = (current_pose['position'].y - pose.position.y) / steps
-        ik_delta.position.z = (current_pose['position'].z - pose.position.z) / steps
-        ik_delta.orientation.x = (current_pose['orientation'].x - pose.orientation.x) / steps
-        ik_delta.orientation.y = (current_pose['orientation'].y - pose.orientation.y) / steps
-        ik_delta.orientation.z = (current_pose['orientation'].z - pose.orientation.z) / steps
-        ik_delta.orientation.w = (current_pose['orientation'].w - pose.orientation.w) / steps
-        for d in range(int(steps), -1, -1):
-            if rospy.is_shutdown():
-                return
-            ik_step = Pose()
-            ik_step.position.x = d*ik_delta.position.x + pose.position.x
-            ik_step.position.y = d*ik_delta.position.y + pose.position.y
-            ik_step.position.z = d*ik_delta.position.z + pose.position.z
-            ik_step.orientation.x = d*ik_delta.orientation.x + pose.orientation.x
-            ik_step.orientation.y = d*ik_delta.orientation.y + pose.orientation.y
-            ik_step.orientation.z = d*ik_delta.orientation.z + pose.orientation.z
-            ik_step.orientation.w = d*ik_delta.orientation.w + pose.orientation.w
-            joint_angles = self._limb.ik_request(ik_step, self._tip_name)
-            if joint_angles:
-                self._limb.set_joint_positions(joint_angles)
-            else:
-                rospy.logerr("No Joint Angles provided for move_to_joint_positions. Staying put.")
-            r.sleep()
-        rospy.sleep(1.0)
+        self._moveit_interface.move_to_pose_target(target_pose)
 
-    def pick(self, pose):
+    def pick(self, sim_object):
         if rospy.is_shutdown():
             return
-        # open the gripper
+
         self.gripper_open()
-        # servo above pose
-        self._approach(pose)
-        # servo to pose
-        self._servo_to_pose(pose)
+        self._approach(sim_object)
+        target_pose = sim_object.get_current_pose()
+        rospy.loginfo("Pick pose: {}".format(target_pose))
+        self._moveit_interface.move_to_pose_target(target_pose)
         if rospy.is_shutdown():
             return
         # close gripper
         self.gripper_close()
         # retract to clear object
-        self._retract()
+        self._retract(sim_object)
 
-    def place(self, pose):
+    def place(self, sim_object, target_pose):
+        # Need to set target pose.
         if rospy.is_shutdown():
             return
         # servo above pose
-        self._approach(pose)
+        self._approach(sim_object)
         # servo to pose
-        self._servo_to_pose(pose)
+
+        self._moveit_interface.move_to_pose_target(target_pose)
         if rospy.is_shutdown():
             return
         # open the gripper
         self.gripper_open()
+        sim_object.pose.position = target_pose.position
         # retract to clear object
-        self._retract()
+        self._retract(sim_object)
 
-def load_gazebo_models(table_pose=Pose(position=Point(x=0.75, y=0.0, z=0.0)),
-                       table_reference_frame="world",
-                       block_pose=Pose(position=Point(x=0.4225, y=0.1265, z=0.7728)),
-                       block_reference_frame="world",
-                       green_bowl_pose=Pose(position=Point(x=0.3225, y=0.1065, z=0.7728)),
-                       green_bowl_reference_frame="world"):
-    # Get Models' Path
-    model_path = rospkg.RosPack().get_path('planning_sim_demo')+"/models/"
-    
-    # Load Table SDF
-    table_xml = ''
-    with open (model_path + "cafe_table/model.sdf", "r") as table_file:
-        table_xml=table_file.read().replace('\n', '')
-
-    green_bowl_xml = ''
-    with open (model_path + "green_bowl/model.sdf", "r") as green_bowl_file:
-        green_bowl_xml=green_bowl_file.read().replace('\n', '')
-    
-    # Load Red Block URDF
-    red_block_xml = ''
-    with open (model_path + "red_block/model.urdf", "r") as red_block_file:
-        red_block_xml=red_block_file.read().replace('\n', '')
-    
-    # Load Green Block URDF
-    green_block_xml = ''
-    with open (model_path + "green_block/model.urdf", "r") as green_block_file:
-        green_block_xml=green_block_file.read().replace('\n', '')
-
-    # Spawn Table SDF
-    rospy.wait_for_service('/gazebo/spawn_sdf_model')
-    try:
-        spawn_sdf = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
-        resp_sdf = spawn_sdf("cafe_table", table_xml, "/",
-                             table_pose, table_reference_frame)
-    except rospy.ServiceException, e:
-        rospy.logerr("Spawn SDF service call failed: {0}".format(e))
-
-    # Spawn Green Bowl SDF
-    rospy.wait_for_service('/gazebo/spawn_sdf_model')
-    try:
-        spawn_sdf = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
-        resp_sdf = spawn_sdf("green_bowl", green_bowl_xml, "/",
-                             green_bowl_pose, green_bowl_reference_frame)    
-    except rospy.ServiceException, e:
-        rospy.logerr("Spawn SDF service call failed: {0}".format(e))
-
-    # Spawn Red Block URDF
-    rospy.wait_for_service('/gazebo/spawn_urdf_model')
-    try:
-        spawn_urdf = rospy.ServiceProxy('/gazebo/spawn_urdf_model', SpawnModel)
-        resp_urdf = spawn_urdf("red_block", red_block_xml, "/",
-                               block_pose, block_reference_frame)
-    except rospy.ServiceException, e:
-        rospy.logerr("Spawn URDF service call failed: {0}".format(e))
-
-    # Spawn Green Block URDF
-    rospy.wait_for_service('/gazebo/spawn_urdf_model')
-    try:
-        spawn_urdf = rospy.ServiceProxy('/gazebo/spawn_urdf_model', SpawnModel)
-        resp_urdf = spawn_urdf("green_block", green_block_xml, "/",
-                               block_pose, block_reference_frame)
-    except rospy.ServiceException, e:
-        rospy.logerr("Spawn URDF service call failed: {0}".format(e))
+def load_gazebo_models(sim_objects):
+  for obj in sim_objects:
+    obj.load_gazebo_model()
 
 def delete_gazebo_models():
     # This will be called on ROS Exit, deleting Gazebo models
@@ -232,8 +189,10 @@ def delete_gazebo_models():
         resp_delete = delete_model("cafe_table")
         resp_delete = delete_model("red_block")
         resp_delete = delete_model("green_block")
+        resp_delete = delete_model("green_bowl")
+        resp_delete = delete_model("red_bowl")
     except rospy.ServiceException, e:
-        print("Delete Model service call failed: {0}".format(e))
+        rospy.loginfo("Delete Model service call failed: {0}".format(e))
 
 def main():
     """SDK Inverse Kinematics Pick and Place Example
@@ -253,12 +212,24 @@ def main():
     # Load Gazebo Models via Spawning Services
     # Note that the models reference is the /world frame
     # and the IK operates with respect to the /base frame
-    load_gazebo_models()
+
+    # Get Models' Path
+    model_path = rospkg.RosPack().get_path('planning_sim_demo')+"/models/"
+
+    sim_objects = OrderedDict()
+    sim_objects["cafe_table"] = SimulationObject("cafe_table", model_path + "cafe_table/model.sdf", model_type="sdf", pose=Pose(position=Point(x=0.75, y=0.0, z=0.0)))
+    sim_objects["red_block"] = SimulationObject("red_block", model_path + "red_block/model.urdf", model_type="urdf", pose=Pose(position=Point(x=0.4225, y=0.1265, z=0.7725)))
+    sim_objects["green_block"] = SimulationObject("green_block", model_path + "green_block/model.urdf", model_type="urdf", pose=Pose(position=Point(x=0.4225, y=-0.1665, z=0.7725)))
+    sim_objects["green_bowl"] = SimulationObject("green_bowl", model_path + "green_bowl/model.sdf", model_type="sdf", pose=Pose(position=Point(x=0.6000, y=0.1265, z=0.7725)))
+    sim_objects["red_bowl"] = SimulationObject("red_bowl", model_path + "red_bowl/model.sdf", model_type="sdf", pose=Pose(position=Point(x=0.6000, y=-0.1265, z=0.7725)))
+
+    # Load all simulation objects into Gazebo
+    load_gazebo_models(sim_objects.values())
+
     # Remove models from the scene on shutdown
     rospy.on_shutdown(delete_gazebo_models)
 
     limb = 'right'
-    hover_distance = 0.15 # meters
     # Starting Joint angles for right arm
     starting_joint_angles = {'right_j0': -0.041662954890248294,
                              'right_j1': -1.0258291091425074,
@@ -267,35 +238,35 @@ def main():
                              'right_j4':  -0.06703022873354225,
                              'right_j5': 0.3968371433926965,
                              'right_j6': 1.7659649178699421}
-    pnp = PickAndPlace(limb, hover_distance)
+    pnp = PickAndPlace(limb)
     # An orientation for gripper fingers to be overhead and parallel to the obj
     overhead_orientation = Quaternion(
-                             x=-0.00142460053167,
-                             y=0.999994209902,
-                             z=-0.00177030764765,
-                             w=0.00253311793936)
+                             x=-0.011945179403945185,
+                             y=0.9990525677759885,
+                             z=-0.007193759099379768,
+                             w=0.04122532925175943)
     block_poses = list()
     # The Pose of the block in its initial location.
     # You may wish to replace these poses with estimates
     # from a perception node.
     block_poses.append(Pose(
         position=Point(x=0.45, y=0.155, z=-0.129),
-        orientation=overhead_orientation))
+        orientation=copy.deepcopy(overhead_orientation)))
     # Feel free to add additional desired poses for the object.
     # Each additional pose will get its own pick and place.
     block_poses.append(Pose(
         position=Point(x=0.6, y=-0.1, z=-0.129),
-        orientation=overhead_orientation))
+        orientation=copy.deepcopy(overhead_orientation)))
     # Move to the desired starting angles
     print("Running. Ctrl-c to quit")
     pnp.move_to_start(starting_joint_angles)
     idx = 0
     while not rospy.is_shutdown():
-        print("\nPicking...")
-        pnp.pick(block_poses[idx])
-        print("\nPlacing...")
+        rospy.loginfo("\nPicking...")
+        pnp.pick(sim_objects["red_block"])
+        rospy.loginfo("\nPlacing...")
         idx = (idx+1) % len(block_poses)
-        pnp.place(block_poses[idx])
+        pnp.place(sim_objects["red_block"], block_poses[idx])
     return 0
 
 if __name__ == '__main__':
